f067e921cfd17b742ffc5792f73efd2fc0d9ce9c
freeser <freeser@126.com>
2026-04-20 15:47:40 +0800

    增加事务和返回公共Result

--- src/main/java/com/fyfe/controller/EmpController.java
+++ src/main/java/com/fyfe/controller/EmpController.java
@@ -1,12 +1,14 @@
 package com.fyfe.controller;
 
+import com.fyfe.dto.Result;
 import com.fyfe.pojo.Emp;
 import com.fyfe.service.EmpService;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
@@ -15,12 +17,16 @@ public class EmpController {
     EmpService empService;
 
     @GetMapping("/list")
-    public List<Emp> list() {
-        List<Emp> list = empService.list();
-        return list;
+    public Result list() {
+        return Result.buildSuccess(empService.list());
     }
     @GetMapping("/page")
-    public PageInfo<Emp> page(Integer pageNum, Integer pageSize) {
-        return empService.pageList(pageNum, pageSize);
+    public Result page(Integer pageNum, Integer pageSize) {
+        return Result.buildSuccess(empService.pageList(pageNum, pageSize));
+    }
+
+    @PostMapping("/save")
+    public Result save(@RequestBody Emp emp) {
+        return Result.buildSuccess(empService.insert(emp));
     }
 }
--- src/main/java/com/fyfe/service/impl/EmpServiceImpl.java
+++ src/main/java/com/fyfe/service/impl/EmpServiceImpl.java
@@ -7,10 +7,13 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class EmpServiceImpl implements EmpService {
 
     @Autowired
@@ -26,4 +29,12 @@ public class EmpServiceImpl implements EmpService {
         PageHelper.startPage(pageNum,pageSize); //这个语句必须放在selectAll查询前面
         return new PageInfo<>(empMapper.selectAll());
     }
+
+    @Override
+    public int insert(Emp emp) {
+        int i = empMapper.insert(emp);
+        int a = 1/0;
+        return i;
+    }
+
 }
